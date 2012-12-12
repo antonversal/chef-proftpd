910104bcca75312049ee3d7a45d53882ce572dcb
Anton Versal <ant.ver@gmail.com>
2012-12-12 10:23:57 +0200

    updated metadata and default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,10 @@
 default[:proftpd][:version] = "1.3.4b"
 
 set[:proftpd][:dir]     = "/etc/proftpd"
-set[:proftpd][:binary]  = "/usr/sbin/proftpd"
 set[:proftpd][:log_dir] = "/var/log/proftpd"
 
 default[:proftpd][:user] = "proftpd"
-default[:proftpd][:ftp_user] = "ftp"
 
 default[:proftpd][:port]                 = 21
-default[:proftpd][:default_address]      = ipaddress
 default[:proftpd][:max_instances]        = 30
 default[:proftpd][:umask]                = "022"
--- metadata.rb
+++ metadata.rb
@@ -6,11 +6,11 @@ version           "0.0.1"
 
 recipe "proftpd::source", "Installs proftpd from source and sets up configuration"
 
-%w{ ubuntu debian centos redhat fedora }.each do |os|
+%w{ ubuntu }.each do |os|
   supports os
 end
 
-%w{ build-essential runit }.each do |cb|
+%w{ build-essential }.each do |cb|
   depends cb
 end
 
@@ -19,8 +19,32 @@ attribute "proftpd/dir",
   :description => "Location of proftpd configuration files",
   :default => "/etc/proftpd"
 
+attribute "proftpd/log_dir",
+  :display_name => "proftpd log directory",
+  :description => "Location of proftpd log files",
+  :default => "/var/log/proftpd"
 
-attribute "proftpd/binary",
-  :display_name => "Proftpd Binary",
-  :description => "Location of the proftpd server binary",
-  :default => "/usr/sbin/proftpd"
+attribute "proftpd/contrib_modules",
+  :display_name => "contrib modules",
+  :description => "list of proftpd contrib modules",
+  :default => nil
+
+attribute "proftpd/user",
+  :display_name => "Proftpd user",
+  :description => "User that runs proftpd service",
+  :default => "proftpd"
+
+attribute "proftpd/port",
+  :display_name => "Server port",
+  :description => "ftp server port, usually 21",
+  :default => 21
+
+attribute "proftpd/max_instances",
+  :display_name => "Maximum instances",
+  :description => "Maximum number of child processes",
+  :default => 30
+
+attribute "proftpd/umask",
+  :display_name => "Umask",
+  :description => "Umask",
+  :default => "022"
