c56703a0d29734eea6b32341cc1b7531c9db2a9d
Anton Versal <ant.ver@gmail.com>
2012-12-12 10:00:39 +0200

    added default variables

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,10 @@ set[:proftpd][:dir]     = "/etc/proftpd"
 set[:proftpd][:binary]  = "/usr/sbin/proftpd"
 set[:proftpd][:log_dir] = "/var/log/proftpd"
 
-set[:proftpd][:user] = "proftpd"
-set[:proftpd][:ftp_user] = "ftp"
+default[:proftpd][:user] = "proftpd"
+default[:proftpd][:ftp_user] = "ftp"
+
+default[:proftpd][:port]                 = 21
+default[:proftpd][:default_address]      = ipaddress
+default[:proftpd][:max_instances]        = 30
+default[:proftpd][:umask]                = "022"
